cb1c18be20355d8d57a5e39043acfc6082f8b9e3
Aman <a8285443826@gmail.com>
2020-09-10 03:30:26 +0530

    documented files

--- src/components/CustomCard/CustomCard.jsx
+++ src/components/CustomCard/CustomCard.jsx
@@ -2,41 +2,51 @@ import React, { useState } from "react";
 import { Card, Avatar, Modal, Button, DatePicker, Table, Row } from "antd";
 import "./CustomCard.css";
 
+//import members for mock members
 import members from "../../constant/member";
 
 const { Column } = Table;
 const { Meta } = Card;
 
 const CustomCard = () => {
-    //
+    //Open close modal
     const [visible, setVisible] = useState(false);
+
+    //Selects member whose activity to be shown
     const [activity, setActivity] = useState([]);
 
+    //Close modal
     const handleOk = () => {
         console.log("ok");
         setVisible(false);
     };
 
+    //Select user
     const clickHandler = (ind) => {
         setActivity(members[ind].activity_periods);
         setVisible(true);
     };
 
+    //Filter activity based on date
     const dateHandler = (data, dateString) => {
-        console.log(dateString, data);
+        // console.log(dateString, data);
         const newActivity = activity.filter((elem) => {
             const d = new Date(elem.start_time.slice(0, 11));
             const date = d.getDate();
             const month = d.getMonth() + 1;
             const year = d.getFullYear();
+
+            //format date for comparison
             const formattedDate = `${year}-${
                 month <= 9 ? `0${month}` : month
             }-${date <= 9 ? `0${date}` : date}`;
-            console.log(formattedDate, elem.start_time, dateString);
+            // console.log(formattedDate, elem.start_time, dateString);
             return formattedDate === dateString;
         });
+
+        //set activity for the given date
         setActivity(newActivity);
-        console.log(newActivity);
+        // console.log(newActivity);
     };
 
     return (
